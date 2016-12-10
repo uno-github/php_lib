e5fd0cb449ceb4119d6582d220f06fb6db560eb5
uno <unotest@hotmail.co.jp>
2016-12-10 23:30:09 +0900

    mkdir_r修正

--- utility.php
+++ utility.php
@@ -7,13 +7,12 @@ class Utility{
     /**
      * 再帰的なmkdir
      */
-    public function mkdir_r($dir_name, $mode = 0777){
-        if( is_dir($dir_name) ){return true; }
-        if( is_dir(dirname($dir_name)) ){
-            return (mkidr($dir_name) && chmod($dir_name, $mode) );
-        }else{
-            return mkdir_r(dirname($dir_name));
+    public static function mkdir_r($dir_name, $mode = 0777){
+        if( is_dir($dir_name) ){return true;}
+        if( !is_dir(dirname($dir_name)) ){
+            if( !Utility::mkdir_r(dirname($dir_name))){return false;}
         }
+        return ( mkdir($dir_name) && chmod($dir_name, $mode) );
     }
 
 
